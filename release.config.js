ced6246e56a8cf4af2594d616997093ddd25af64
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-27 18:28:49 +0200

    ci: add semanti release config

---
+++ release.config.js
@@ -0,0 +1,20 @@
+const config = require('semantic-release-preconfigured-conventional-commits')
+
+const releaseBranches = ["master"]
+config.branches = releaseBranches
+config.plugins.push(
+    ["@semantic-release/exec", {
+    }],
+    ["@semantic-release/github", {
+        "assets": [
+            // Change this path if your pdf has another name.
+            { "path": "distributed-systems/thesis-main.pdf" },
+            { "path": "smart-city/report.pdf" },
+        ]
+    }],
+    ["@semantic-release/git", {
+        "assets": ["CHANGELOG.md", "package.json"],
+        "message": "chore(release)!: [skip ci] ${nextRelease.version} released"
+    }],
+)
+module.exports = config
